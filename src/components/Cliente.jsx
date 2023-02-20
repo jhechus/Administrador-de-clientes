23b6a84cce39f04434d996869f4a706690ab3701
jhechus <jess.osm1412@gmail.com>
2023-02-20 11:25:10 -0600

    Interando sobre clientes y mostrando la informacion

---
+++ src/components/Cliente.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+function Cliente({cliente}) {
+
+const {nombre, empresa, email, telefono, id} = cliente
+  
+return (
+    <tr>
+        <td className=" p-6">
+            {nombre}
+        </td>
+    </tr>
+  )
+}
+
+export default Cliente
--- src/main.jsx
+++ src/main.jsx
@@ -4,7 +4,7 @@ import './index.css'
 import { createBrowserRouter, RouterProvider } from 'react-router-dom'
 import Layout from './components/Layout'
 import Nuevocliente from './pages/Nuevocliente'
-import Index, {loader} from './pages/Index'
+import Index, {loader as clientesLoader, loader} from './pages/Index'
 
 const router = createBrowserRouter([
   {
@@ -13,7 +13,8 @@ const router = createBrowserRouter([
     children: [
       {
         index: true,
-        element: <Index />
+        element: <Index />,
+        loader: clientesLoader
       },
       {
         path:'/clientes/nuevo',
