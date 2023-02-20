7ec398ce5d09a6c954829fa32366ed47451a4d9b
jhechus <jess.osm1412@gmail.com>
2023-02-20 11:48:38 -0600

    agregando los primeros pasos para el formulario

--- src/pages/Nuevocliente.jsx
+++ src/pages/Nuevocliente.jsx
@@ -1,10 +1,27 @@
-import React from 'react'
+import { useNavigate } from "react-router-dom"
 
 function Nuevocliente() {
+
+const navigate = useNavigate()
+
   return (
-    <div>
-      Nuevo Cliente
-    </div>
+    <>
+      <h1 className=' font-black text-4xl text-blue-900 '> Nuevo Cliente </h1>
+      <p className=' mt-3 '> LLena todos los campos para un nuevo registro </p>
+
+      <div className=' flex justify-end'>
+        <button
+          className=' bg-blue-800 text-white px-3 py-1 font-bold uppercase'
+          onClick={() => navigate('/')}
+        >
+          Volver
+        </button>
+      </div>
+
+      <div className=" bg-white shadow rounded-md md:w-3/4 mx-auto px-5 py-10">
+        <p>Formulario aqui </p>
+      </div>
+    </>
   )
 }
 
